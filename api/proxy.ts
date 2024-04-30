68706f17f872d7a48e503f0271dac45c5c5e8c3c
CRYSTAL <110715851+n3-n2-n1@users.noreply.github.com>
2024-04-30 00:34:41 -0300

    fix handlers

--- api/proxy.ts
+++ api/proxy.ts
@@ -2,7 +2,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import fetch from 'node-fetch';
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-  const url = 'https://api.bcra.gob.ar/estadisticas/...'; // URL de la API que quieres acceder
+  const url = 'https://api.bcra.gob.ar/estadisticas/v1/principalesvariables'; // URL de la API que quieres acceder
   const apiResponse = await fetch(url);
   const data = await apiResponse.json();
 
