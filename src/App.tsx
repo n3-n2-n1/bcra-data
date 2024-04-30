aac970a2159c6d08c7c3bff014144fb129a477db
CRYSTAL <110715851+n3-n2-n1@users.noreply.github.com>
2024-04-30 01:25:42 -0300

    new loader!

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,24 @@ import { Content } from "./components/Content/Content";
 import { Items } from "./components/Items/Items";
 import { useState } from "react";
 import Loader from "./components/Loader/Loader";
+import { useEffect } from "react";
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    // Aquí iría tu llamada a la API o lógica para obtener los datos de los items
+    fetchItems().then(data => {
+      setItems(data); // Actualiza el estado con los items obtenidos
+      setIsLoading(false); // Establece isLoading en false para ocultar el loader
+    });
+  }, []); // El array vacío asegura que el efecto se ejecute solo una vez después del montaje inicial
+
+  // Simula una llamada a la API
+  const fetchItems = async () => {
+    // Simula un retraso para demostrar el loader
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    return [{ id: 1, name: 'Item 1' }, { id: 2, name: 'Item 2' }]; // Datos simulados
+  };
   return (
     <>
       {isLoading ? (
