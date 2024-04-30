cd3365f767978deac817a174df5664f408e4821d
CRYSTAL <110715851+n3-n2-n1@users.noreply.github.com>
2024-04-30 01:50:05 -0300

    new loader for comp

--- src/components/DataWidget/dataWidget.tsx
+++ src/components/DataWidget/dataWidget.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import Loader from "../Loader/Loader";
 export const EconomicDataWidget = () => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
@@ -7,6 +7,7 @@ export const EconomicDataWidget = () => {
   const [filteredData, setFilteredData] = useState([]);
   const [selectedItem, setSelectedItem] = useState(null);
   const [orderByDate, setOrderByDate] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const apiUrl = import.meta.env.VITE_API_URL;
   const local = import.meta.env.VITE_API_LOCAL;
@@ -17,6 +18,7 @@ export const EconomicDataWidget = () => {
       .then((data) => {
         setData(data.results); // Guarda los datos recibidos en el estado
         setFilteredData(data.results); // Inicialmente no hay filtro aplicado
+        setIsLoading(false); // Establece isLoading en false para ocultar el loader
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
@@ -118,84 +120,90 @@ export const EconomicDataWidget = () => {
   ];
 
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 py-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-3">
-        {importantData.map((item, index) => (
-          <div
-            key={index}
-            className={`${
-              cardColors[index % cardColors[0].length]
-            } shadow-lg rounded-lg p-4 mb-2 text-white flex flex-col justify-between`}
-            style={{ minHeight: "10rem" }} // Establece una altura mínima para las tarjetas
-          >
-            <h3 className="text-xl font-semibold select-none">
-              {item.descripcion}
-            </h3>
-            <div>
-              {" "}
-              {/* Contenedor para el valor y la fecha */}
-              <p className="font-bold text-3xl pt-4">{item.valor}</p>
-              <p>Valor al {item.fecha}</p>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 py-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-3">
+            {importantData.map((item, index) => (
+              <div
+                key={index}
+                className={`${
+                  cardColors[index % cardColors[0].length]
+                } shadow-lg rounded-lg p-4 mb-2 text-white flex flex-col justify-between`}
+                style={{ minHeight: "10rem" }} // Establece una altura mínima para las tarjetas
+              >
+                <h3 className="text-xl font-semibold select-none">
+                  {item.descripcion}
+                </h3>
+                <div>
+                  {" "}
+                  {/* Contenedor para el valor y la fecha */}
+                  <p className="font-bold text-3xl pt-4">{item.valor}</p>
+                  <p>Valor al {item.fecha}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-between mb-4 items-center">
+            {/* Selector de filtros y toggle juntos para responsividad */}
+            <div className="flex flex-1 items-center space-x-2">
+              <div className="w-full">
+                <select
+                  onChange={handleFilterChange}
+                  className="block py-2.5 px-3 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+                >
+                  <option value="all">Todos</option>
+                  <option value="tasa">Tasas</option>
+                  <option value="índice">Índices</option>
+                  <option value="efectivo">Efectivo</option>
+                  <option value="depósito">Depósito</option>
+                  <option value="inflación">Inflación</option>
+                </select>
+              </div>
+              <div className="flex items-center space-x-2">
+                <label htmlFor="orderByDate" className="text-sm text-gray-700">
+                  Ordenar por fecha:
+                </label>
+                <input
+                  id="orderByDate"
+                  type="checkbox"
+                  checked={orderByDate}
+                  onChange={(e) => setOrderByDate(e.target.checked)}
+                  className="cursor-pointer"
+                />
+              </div>
             </div>
           </div>
-        ))}
-      </div>
-      <div className="flex justify-between mb-4 items-center">
-        {/* Selector de filtros y toggle juntos para responsividad */}
-        <div className="flex flex-1 items-center space-x-2">
-          <div className="w-full">
-            <select
-              onChange={handleFilterChange}
-              className="block py-2.5 px-3 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
-            >
-              <option value="all">Todos</option>
-              <option value="tasa">Tasas</option>
-              <option value="índice">Índices</option>
-              <option value="efectivo">Efectivo</option>
-              <option value="depósito">Depósito</option>
-              <option value="inflación">Inflación</option>
-            </select>
-          </div>
-          <div className="flex items-center space-x-2">
-            <label htmlFor="orderByDate" className="text-sm text-gray-700">
-              Ordenar por fecha:
-            </label>
-            <input
-              id="orderByDate"
-              type="checkbox"
-              checked={orderByDate}
-              onChange={(e) => setOrderByDate(e.target.checked)}
-              className="cursor-pointer"
-            />
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full">
+            {filteredData.map((item, index) => (
+              <div
+                key={index}
+                className={`bg-white shadow-md rounded-lg p-4 border-l-4 ${getCategoryColor(
+                  item.descripcion
+                )} hover:shadow-lg transition-shadow duration-300 relative select-none`}
+                onClick={() => toggleItem(index)}
+                onMouseEnter={() => toggleItem(index)}
+                onMouseLeave={() => setSelectedItem(null)}
+              >
+                <h3 className="text-xl font-semibold mb-2 text-black select-none">
+                  {item.descripcion}
+                </h3>
+                <p className="text-black font-bold text-2xl">{item.valor}</p>
+                <p className="text-gray-500 pt-4">Valor al {item.fecha}</p>
+                {selectedItem === index && (
+                  <div className="absolute top-0 left-0 right-0 bottom-0 bg-white bg-opacity-90 flex items-center justify-center p-4 select-none">
+                    <p className="text-gray-700 text-lg font-semibold">
+                      {infoWidget(item.descripcion)}
+                    </p>
+                  </div>
+                )}
+              </div>
+            ))}
           </div>
         </div>
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full">
-        {filteredData.map((item, index) => (
-          <div
-            key={index}
-            className={`bg-white shadow-md rounded-lg p-4 border-l-4 ${getCategoryColor(
-              item.descripcion
-            )} hover:shadow-lg transition-shadow duration-300 relative select-none`}
-            onClick={() => toggleItem(index)}
-            onMouseEnter={() => toggleItem(index)}
-            onMouseLeave={() => setSelectedItem(null)}
-          >
-            <h3 className="text-xl font-semibold mb-2 text-black select-none">
-              {item.descripcion}
-            </h3>
-            <p className="text-black font-bold text-2xl">{item.valor}</p>
-            <p className="text-gray-500 pt-4">Valor al {item.fecha}</p>
-            {selectedItem === index && (
-              <div className="absolute top-0 left-0 right-0 bottom-0 bg-white bg-opacity-90 flex items-center justify-center p-4 select-none">
-                <p className="text-gray-700 text-lg font-semibold">
-                  {infoWidget(item.descripcion)}
-                </p>
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
-    </div>
+      )}
+    </>
   );
 };
